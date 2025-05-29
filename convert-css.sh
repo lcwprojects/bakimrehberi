3ba561c8af6a58d9ff216d8be119cc63a049f6c6
Erhan Öztürk <erhan@unknown3a28c5ebac11.home>
2025-05-29 13:36:13 +0300

    İlk yükleme

--- convert-css.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cat assets/css/modal-styles.css | sed "s/\(\.[a-zA-Z0-9][a-zA-Z0-9_-]*\)/\#bakimrehberi \1/g" | sed "s/\(#[a-zA-Z0-9][a-zA-Z0-9_-]*\)/\#bakimrehberi \1/g" > assets/css/modal-styles-new.css
